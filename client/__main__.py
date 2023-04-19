71c94b48ba8fa85c0ad29d7be05a76932c5abb93
AntonioArtigas <48927276+AntonioArtigas@users.noreply.github.com>
2023-04-18 20:24:19 -0400

    Start implementing client to server requests

--- client/__main__.py
+++ client/__main__.py
@@ -1,10 +1,46 @@
+import argparse
+import json
+
+from websockets import InvalidURI
 from websockets.sync.client import ClientConnection, connect
 
 if __name__ == "__main__":
-    port = 42069
-    url = f"ws://localhost:{port}/connect"
-    print(f"connecting to {url}!")
-    with connect(url) as ws:
-        ws: ClientConnection
-        ws.send("Hello, world!")
-        print("Sent hello world!")
+    parser = argparse.ArgumentParser("client", "client <host> <port> <channel>")
+    parser.add_argument("host")
+    parser.add_argument("port", type=int)
+    parser.add_argument("channel")
+    parser.add_argument("-u", "--user", help="Specify user name.", default="test-user")
+
+    args = parser.parse_args()
+
+    host: str = args.host
+    port: int = args.port
+    username: str = args.user
+    url = f"ws://{host}:{port}/connect"
+    print(f"Connecting to '{url}'...")
+    try:
+        with connect(url) as ws:
+            ws: ClientConnection
+
+            # Login into server.
+            login = {
+                "type": "login",
+                "data": {
+                    "username": username
+                }
+            }
+            ws.send(json.dumps(login))
+
+            message_string: str = input("Input a message: ")
+            message = {
+                "type": "message",
+                "data": {
+                    "author": "test-user",
+                    "content": message_string
+                }
+            }
+            ws.send(json.dumps(message))
+    except InvalidURI:
+        print(f"URL {url} isn't a valid URI.")
+    except OSError:
+        print("Could not connect via TCP.")
--- server/__main__.py
+++ server/__main__.py
@@ -1,34 +1,91 @@
+import argparse
 import asyncio
+import json
+from typing import Union
+
 from tornado.web import Application
 from tornado.websocket import WebSocketHandler
 
-from typing import Union, Optional, Awaitable
+from .request import Request, RequestFactory
+
+
+class Client:
+    def __init__(
+            self, server: "ChatteringApplication", connection: "ConnectHandler"
+    ) -> None:
+        self.name = ""
+        self.server = server
+        self.connection = connection
+
+    def handle_request(self, message: Request):
+        print(f"handle_request: {message}")
+
 
 class ConnectHandler(WebSocketHandler):
+    server: "ChatteringApplication"
+    client: Client
+    request_factory: RequestFactory
+
+    def initialize(self, server: "ChatteringApplication"):
+        self.server = server
+        self.client = Client(server, self)
+        self.request_factory = RequestFactory()
+
     def open(self):
-        print("Someone has connected!")
-        pass
+        print(f"Connection from {self.request.remote_ip} opened.")
+        self.server.add_client(self)
 
     def on_close(self):
-        print("Someone has left.")
-        pass
+        print(f"Connection from {self.request.remote_ip} closed.")
+        self.server.remove_client(self)
 
-    def on_message(self, message: Union[str, bytes]) -> Optional[Awaitable[None]]:
-        print(f"Someone sent a message of {message}")
+    def on_message(self, raw_message: Union[str, bytes]):
+        print(f"{self.request.remote_ip} sent {raw_message}")
+        try:
+            json_message = json.loads(raw_message)
+            request = self.request_factory.deserialize_request(json_message)
+            try:
+                self.client.handle_request(request)
+            except ValueError:
+                print(f"Unknown request type '{json_message['ty']}'")
+        except json.JSONDecodeError:
+            print("Bad json request. Ignoring.")
 
 
 class ChatteringApplication(Application):
+    connections: dict[ConnectHandler, Client]
+
     def __init__(self) -> None:
-        handlers = [(r"/connect", ConnectHandler)]
+        handlers = [
+            (r"/connect", ConnectHandler, dict(server=self)),
+        ]
         super().__init__(handlers)
+
+        self.connections = {}
+
+    def add_client(self, connection: ConnectHandler):
+        self.connections[connection] = Client(self, connection)
+
+    def remove_client(self, connection: ConnectHandler):
+        del self.connections[connection]
 
 
 async def main():
-    port = 42069
+    parser = argparse.ArgumentParser("server", "server <port>")
+    parser.add_argument("port", type=int)
+
+    args = parser.parse_args()
+
+    port: int = args.port
     app = ChatteringApplication()
-    print(f"Listening on port {port}")
+    print(f"Listening on port {port}.")
     app.listen(port)
     await asyncio.Event().wait()
 
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        # Catch to prevent showing stack trace to user.
+        print("Server stopped.")
